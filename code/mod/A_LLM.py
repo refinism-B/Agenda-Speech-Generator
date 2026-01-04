689b0270e4da584c659da16bd7ef9d9af48d81ac
Bevis <add412@gmail.com>
2026-01-04 22:22:34 +0800

    <modify>：使用pydantic規範輸出格式

--- code/mod/A_LLM.py
+++ code/mod/A_LLM.py
@@ -1,17 +1,33 @@
 import os
+from datetime import time
+from typing import List
 
+import pandas as pd
 from dotenv import load_dotenv
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser, PydanticOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from mod.O_prompt import SYSTEM, USER
-import pandas as pd
-
+from pydantic import BaseModel, Field
 
 load_dotenv()
 USER_AGENT = os.environ.get("USER_AGENT")
 api_key = os.environ.get("GOOGLE_API")
+
+
+class SpeechAgenda(BaseModel):
+    title: str = Field(description="演講主題")
+    start_time: time = Field(description="開始時間")
+    end_time: time = Field(description="結束時間")
+    speaker_title: str = Field(description="講者職稱與單位", default="無")
+    speaker_name: str = Field(description="講者姓名", default="無")
+    script: str = Field(description="司儀稿內容")
+
+
+class AgendaSchedule(BaseModel):
+    event_name: str = Field(description="活動名稱")
+    agendas: List[SpeechAgenda] = Field(description="議程清單")
 
 
 def load_web_info(url: str):
@@ -38,11 +54,15 @@ def create_prompt_template():
 
 def create_prompt_data(web_content):
     json_parser = JsonOutputParser()
-    json_prompt = json_parser.get_format_instructions()
+
+    pydantic_parser = PydanticOutputParser(
+        pydantic_object=AgendaSchedule
+    )
+    pydantic_prompt = pydantic_parser.get_format_instructions()
 
     return json_parser, {
         "web_content": web_content,
-        "json_parser": json_prompt
+        "pydantic_parser": pydantic_prompt
     }
 
 
@@ -52,10 +72,5 @@ def get_tokens_info(ai_message):
         return tokens['input_tokens'], tokens['output_tokens']
 
 
-def transform_to_df(response):
-    data = {
-        "時間": list(response.keys()),
-        "講稿": list(response.values())
-    }
-
-    return pd.DataFrame(data)
+def transform_to_df(response, key):
+    return pd.DataFrame(response[key])
